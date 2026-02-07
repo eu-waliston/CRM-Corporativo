6579facc805a7c79b309550ba60ca965a8379714
Waliston <wesantosdev@gmail.com>
2026-02-07 16:10:56 -0300

    toggle buton Dark mode TODO

--- crm-corporativo/src/components/layout/MainLayout.js
+++ crm-corporativo/src/components/layout/MainLayout.js
@@ -323,7 +323,7 @@ const MainLayout = () => {
             </div>
 
             {/* Theme Toggle */}
-            <button
+            {/* <button
               onClick={toggleTheme}
               className="p-1.5 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-md transition-colors"
               title={
@@ -337,7 +337,7 @@ const MainLayout = () => {
               ) : (
                 <MoonIcon className="w-4 h-4" />
               )}
-            </button>
+            </button>*/}
 
             {/* Notifications */}
             <NotificationsDropdown />
